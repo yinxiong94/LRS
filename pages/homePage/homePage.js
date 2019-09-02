a7c3cca1d5d78185e6328cf0593b9ad2ed28e986
hongliping123 <49940450+hongliping123@users.noreply.github.com>
2019-09-02 17:36:20 +0800

    初始化

---
+++ pages/homePage/homePage.js
@@ -0,0 +1,75 @@
+// pages/homePage/homePage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //首页轮播图片
+    bannerPicArray:
+    [
+        { imgUrl: "http://images.baixingliangfan.cn/advertesPicture/20180407/20180407175040_1780.jpg"},
+        { imgUrl: "http://images.baixingliangfan.cn/advertesPicture/20180407/20180407175111_9509.jpg"},
+        { imgUrl: "http://images.baixingliangfan.cn/advertesPicture/20180407/20180407175142_6947.jpg"}
+    ],
+    autoplay:true,
+    interval:2000,
+    scrollX:true,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(this.data.bannerPicArray)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
