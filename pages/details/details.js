240e715598fd2a209a4911d1c43eddf8e8d624d7
hongliping123 <49940450+hongliping123@users.noreply.github.com>
2019-09-05 09:40:30 +0800

    新增详情页面

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,39 +1,18 @@
-// pages/About_agency/About_agency.js
-var that
+// pages/details/details.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    color:"rgba(225, 147, 46, 1)",
-    autoplay: true,
-    interval: 2000,
-    dots:true,
-    indicator:"RGBA(255, 255, 255, 1)",
-    curr:0,
-    RgbColor:""
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-
-  handoujump(e){
-    that = this
-    that.setData({
-      curr: e.currentTarget.dataset.curr
-    })
-  },
-
-   randomRgbColor() { //随机生成RGB颜色
-    var r = Math.floor(Math.random() * 256); //随机生成256以内r值
-    var g = Math.floor(Math.random() * 256); //随机生成256以内g值
-    var b = Math.floor(Math.random() * 256); //随机生成256以内b值
-    return `rgb(${r},${g},${b})`; //返回rgb(r,g,b)格式颜色
-  },
   onLoad: function (options) {
-    
+
   },
 
   /**
@@ -47,10 +26,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    that = this
-    that.setData({
-      RgbColor: this.randomRgbColor()
-    })
+
   },
 
   /**
